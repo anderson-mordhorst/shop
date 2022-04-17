b06406413121c8cab32321da5d20f6d52aa1811a
anderson-mordhorst <anderson.mordhorst@gmail.com>
2022-04-17 17:58:03 -0300

    Adicionando config de receiver das mensagens do kafka

---
+++ shop-validator/src/main/java/com/santana/events/ReceiveKafkaMessage.java
@@ -0,0 +1,64 @@
+package com.santana.events;
+
+import com.santana.dto.ShopDTO;
+import com.santana.dto.ShopItemDTO;
+import com.santana.model.Product;
+import com.santana.repository.ProductRepository;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ReceiveKafkaMessage {
+    
+    private static final String SHOP_TOPIC_NAME = "SHOP_TOPIC";
+    private static final String SHOP_TOPIC_EVENT_NAME = "SHOP_TOPIC_EVENT";
+
+    private final ProductRepository repository;
+    private final KafkaTemplate<String, ShopDTO> kafkaTemplate;
+
+    @KafkaListener(topics = SHOP_TOPIC_NAME, groupId = "group")
+    public void listenShopTopis(ShopDTO dto){
+        try{
+            log.info("Compra recebida no tópico {}", dto.getIdentifier());
+            boolean success = true;
+            for(ShopItemDTO item: dto.getItems()) {
+                Product product = repository.findByIdentifier(item.getProductIdentifier());
+                if(!isValidShop(item, product)) {
+                    shopError(dto);
+                    success = false;
+                    break;
+                }
+            }
+
+            if(success) {
+                shopSuccess(dto);                    
+            }            
+        }
+        catch(Exception e) {
+            log.error("Erro no processamento da compra {}", dto.getIdentifier());
+        }
+    }
+
+    private void shopSuccess(ShopDTO dto) {
+        log.info("Compra efetuada com sucesso {}", dto.getIdentifier());
+        dto.setStatus("SUCCESS");
+        kafkaTemplate.send(SHOP_TOPIC_EVENT_NAME, dto);        
+    }
+
+    private void shopError(ShopDTO dto) {
+        log.info("Erro no processamento da compra {}", dto.getIdentifier());
+        dto.setStatus("ERROR");
+        kafkaTemplate.send(SHOP_TOPIC_EVENT_NAME, dto);
+    }
+
+    private boolean isValidShop(ShopItemDTO item, Product product) {
+        return product != null && product.getAmount() >= item.getAmount();
+    }
+}
