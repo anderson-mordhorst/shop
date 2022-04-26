8a93d97911fc864530b2ae685e7db9925a9c5daa
anderson-mordhorst <anderson.mordhorst@gmail.com>
2022-04-25 22:21:26 -0300

    Adicionado projeto shop-python: consumidor Kafka em python

---
+++ shop-python/src/consumer.py
@@ -0,0 +1,34 @@
+import threading
+import json
+import logging
+from kafka import KafkaConsumer
+
+
+class Consumer(threading.Thread):
+
+    messages = []
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+
+    def run(self):
+        try:
+            consumer = KafkaConsumer(
+                bootstrap_servers=['localhost:9092'],
+                group_id='group_python',
+                consumer_timeout_ms=30000,
+            )
+            consumer.subscribe(['SHOP_TOPIC_EVENT'])
+            print('Running consumer')
+            while not self.stop_event.is_set():
+                for message in consumer:
+                    print('Receiving message from topic SHOP_TOPIC_EVENT')
+                    self.messages.append(json.loads(message.value.decode('utf-8')))
+            consumer.close()
+        except IOError as exception:
+            print('Read error')
+            logging.error(exception)
+
+    def get_messages(self):
+        return self.messages
